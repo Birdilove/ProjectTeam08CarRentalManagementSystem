44f3adf637cc6c1a6e982eef70fa06511713a753
Prateek Dutt <prateekdutt27@gmail.com>
2020-11-24 00:11:48 -0800

    New Project for Registration.

---
+++ ProjectTeam08CarRegistrationModule/RegistrationForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ProjectTeam08CarRegistrationModule
+{
+    partial class RegistrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelCustomerName = new System.Windows.Forms.Label();
+            this.textBoxCustomerName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelCustomerName
+            // 
+            this.labelCustomerName.AutoSize = true;
+            this.labelCustomerName.Location = new System.Drawing.Point(57, 52);
+            this.labelCustomerName.Name = "labelCustomerName";
+            this.labelCustomerName.Size = new System.Drawing.Size(109, 17);
+            this.labelCustomerName.TabIndex = 0;
+            this.labelCustomerName.Text = "Customer Name";
+            // 
+            // textBoxCustomerName
+            // 
+            this.textBoxCustomerName.Location = new System.Drawing.Point(240, 49);
+            this.textBoxCustomerName.Name = "textBoxCustomerName";
+            this.textBoxCustomerName.Size = new System.Drawing.Size(200, 22);
+            this.textBoxCustomerName.TabIndex = 1;
+            // 
+            // RegistrationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxCustomerName);
+            this.Controls.Add(this.labelCustomerName);
+            this.Name = "RegistrationForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCustomerName;
+        private System.Windows.Forms.TextBox textBoxCustomerName;
+    }
+}
+
